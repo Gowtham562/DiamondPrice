f6c4b77024a53f02ed7868b9da5fe575db4c7e2a
Gowthamarajaa <gowtham.nambiaarorar@gmail.com>
2023-07-09 14:02:58 +0530

    training pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -17,36 +17,69 @@ class DataIngestionconfig:
 
 
 ## create a data ingestion class
+# class DataIngestion:
+#     def __init__(self):
+#         self.ingestion_config=DataIngestionconfig()
+
+#     def initiate_data_ingestion(self):
+#         logging.info('Data Ingestion method starts')
+
+#         try:
+#             df=pd.read_csv(os.path.join('notebooks/data','gemstone.csv'))
+#             logging.info('Dataset read as pandas Dataframe')
+
+#             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+
+#             df.to_csv(self.ingestion_config.raw_data_path,index=False)
+
+#             logging.info("Train test split")
+#             train_set,test_set=train_test_split(df,test_size=0.30,random_state=42)
+
+#             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+#             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+#             logging.info('Ingestion of data is completed')
+
+#             return(
+#                 self.ingestion_config.train_data_path,
+#                 self.ingestion_config.test_data_path
+
+#             )
+
+
+
+#         except Exception as e:
+#             logging.info('Error occured in Data Ingestion config')
+
+
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataIngestionconfig()
+        self.ingestion_config = DataIngestionconfig()
 
     def initiate_data_ingestion(self):
         logging.info('Data Ingestion method starts')
 
         try:
-            df=pd.read_csv(os.path.join('notebooks/data','gemstone.csv'))
+            df = pd.read_csv(os.path.join('notebooks/data', 'gemstone.csv'))
             logging.info('Dataset read as pandas Dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
 
             logging.info("Train test split")
-            train_set,test_set=train_test_split(df,test_size=0.30,random_state=42)
+            train_set, test_set = train_test_split(df, test_size=0.30, random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info('Ingestion of data is completed')
 
-            return(
+            return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-
             )
 
-
-
         except Exception as e:
-            logging.info('Error occured in Data Ingestion config')
+            logging.info('Error occurred in Data Ingestion config')
+            return None, None
--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,7 @@ import sys
 from src.logger import logging
 
 def error_message_detail(e,ed:sys):
-    _,_,exc_tb = error_detail.exc_info()
+    _,_,exc_tb = ed.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
 
     error_message = "Error Script name [{0}] line no [{1}] erroe message [{2}]".format(file_name,exc_tb.tb_lineno,str(e))
